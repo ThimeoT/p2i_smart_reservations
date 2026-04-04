ad39e70132524016d7248af6c01cc7976f30ce12
Thiméo Tonon <ttonon@ensc.fr>
2026-04-04 19:04:34 +0200

    fix : nettoyage d'imports

--- api/src/main/java/com/smart_reservation/api/controller/AuthentificationController.java
+++ api/src/main/java/com/smart_reservation/api/controller/AuthentificationController.java
@@ -2,17 +2,14 @@ package com.smart_reservation.api.controller;
 
 import com.smart_reservation.api.configuration.JwtService;
 import com.smart_reservation.api.dto.response.UtilisateurResponseDto;
-import com.smart_reservation.api.model.Utilisateur;
 
 import com.smart_reservation.api.service.UtilisateurService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
